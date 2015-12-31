43a7c7838e9b2d4018a948750ab5b2ac7085f29c
xuliufeng <798245503@qq.com>
2015-12-31 11:25:15 +0800

    例子提交

--- framework-servlet/src/main/java/com/xulf/framework/servlet/examples/HelloServlet.java
+++ framework-servlet/src/main/java/com/xulf/framework/servlet/examples/HelloServlet.java
@@ -8,10 +8,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
+ *  Example Servlet
  * @author : sankoudai
  * @version : created at 2015/9/7
  */
 public class HelloServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=utf-8");
